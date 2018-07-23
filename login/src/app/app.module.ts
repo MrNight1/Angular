ca1aea66168a97308c20afbce26adc3a8d3a8ed8
CHARMIN-PC\Oscar David <o.d.anota@gmail.com>
2018-07-23 10:26:38 -0500

    Adding libraries for Firebase auth

--- login/src/app/app.module.ts
+++ login/src/app/app.module.ts
@@ -4,6 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MyloginComponent } from './mylogin/mylogin.component';
 
+// For Firebase
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule} from 'angularfire2/auth';
+import { environment } from '../environments/environment';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -11,6 +16,8 @@ import { MyloginComponent } from './mylogin/mylogin.component';
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features
   ],
   providers: [],
   bootstrap: [AppComponent]
