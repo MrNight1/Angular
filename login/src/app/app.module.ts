81abaa1922d9299eefdf22a422d0fb188b7c296a
CHARMIN-PC\Oscar David <o.d.anota@gmail.com>
2018-07-23 09:17:08 -0500

    Creation of mylogin component ng generate component mylogin

--- login/src/app/app.module.ts
+++ login/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MyloginComponent } from './mylogin/mylogin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyloginComponent
   ],
   imports: [
     BrowserModule
